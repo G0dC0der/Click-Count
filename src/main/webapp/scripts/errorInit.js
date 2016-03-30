23b7b5ed94e6a172cd2d8d5fe642ca2127c616e3
PojahnM <bvg196@gmail.com>
2016-03-30 22:24:14 +0200

    Fixes #11. Web page no longer part of this proejct

--- src/main/webapp/scripts/errorInit.js
+++
@@ -1,8 +0,0 @@
-function errorOnload(){
-    $('body').append(dialogs.errorDiv({
-        status: Util.queryParam('status'),
-        url: Util.queryParam('url'),
-        group: Util.queryParam('group'),
-        description: Util.queryParam('description')
-    }).content);
-}
