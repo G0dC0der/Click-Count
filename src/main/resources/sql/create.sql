6ab46b9a99304a2e71219efefa66db25b895ad78
PojahnM <bvg196@gmail.com>
2016-06-30 19:45:25 +0200

    Further SQL improvments. Also added Client

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -2,13 +2,12 @@ DROP DATABASE clickcount_db;
 CREATE DATABASE clickcount_db;
 USE clickcount_db;
 
-CREATE TABLE users
+CREATE TABLE Collaborator
 (
-    Username  VARCHAR(50)  NOT NULL PRIMARY KEY,
+    Username  VARCHAR(50)  PRIMARY KEY,
     Password  VARCHAR(255) NOT NULL,
-    Role      VARCHAR      NOT NULL,
+    Role      VARCHAR(20)  NOT NULL
 );
-INSERT INTO users (user_name, password, role) VALUES ('admin', '95f47c8ba31358731223bdf16b87eecc', 'ADMINISTRATOR');
 
 CREATE TABLE Namespace
 (
@@ -22,6 +21,17 @@ CREATE TABLE URL
     Alias      VARCHAR(256) NOT NULL,
     Link       VARCHAR(256) NOT NULL,
     Clicks     BIGINT UNSIGNED DEFAULT 0,
-    CONSTRAINT URL_PK PRIMARY KEY(Namespace, Alias),
-    CONSTRAINT URL_FK FOREIGN KEY my_fk (Namespace) REFERENCES Namespace(Name)
+    PRIMARY KEY (Namespace, Alias),
+    FOREIGN KEY (Namespace) REFERENCES Namespace(Name)
 );
+
+CREATE TABLE Client
+(
+    Identifier VARCHAR(15) PRIMARY KEY,
+    Expire BIGINT UNSIGNED NOT NULL,
+    Namespace VARCHAR(50) NOT NULL,
+    Alias VARCHAR(255) NOT NULL,
+    FOREIGN KEY (Namespace, Alias) REFERENCES URL(Namespace, Alias)
+);
+
+INSERT INTO Collaborator (Username, Password, Role) VALUES ('admin', '95f47c8ba31358731223bdf16b87eecc', 'ADMINISTRATOR');
